4aa5b322d85a90ca48503870318437be5232600e
Bt08s <68190921+Bt08s@users.noreply.github.com>
2023-11-04 23:55:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import dearpygui.dearpygui as dpg
 
 dpg.create_context()
 
-with dpg.window(tag="Primary Window"):
+with dpg.window(tag="Color picker window"):
     dpg.add_color_picker()
 
 dpg.create_viewport(title='Color Picker by Bt08s', width=600, height=500)
 dpg.setup_dearpygui()
 dpg.show_viewport()
-dpg.set_primary_window("Primary Window", True)
+dpg.set_primary_window("Color picker window", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
